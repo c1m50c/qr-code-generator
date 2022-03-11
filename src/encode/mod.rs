6cf80d2f21d4ac578550e097626a779045d4dabc
c1m50c <terminalityms@gmail.com>
2022-03-11 16:43:37 -0600

    Version Checking & Sizing

--- src/encode/mod.rs
+++ src/encode/mod.rs
@@ -1,7 +1,9 @@
 use image::GrayImage;
 
 
-pub fn generate(message: String) -> GrayImage {
-    let image = GrayImage::new(25, 25);
+pub fn generate(message: String, version: u32) -> GrayImage {
+    let size: u32 = 21 + (4 * (version - 1));
+    let image = GrayImage::new(size, size);
+
     return image;
 }
--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,30 @@ fn main() {
         .filter(|s| s.find('-') == Some(0))
         .collect();
     
+    // Retrieve Version Flags.
+    let version: Vec<String> = flags.iter()
+        .filter(|&s| s.find('-') == Some(0) && s.find('v') == Some(1))
+        .map(|s| s.clone())
+        .collect();
+    
+    // Isolate a singular Version Flag and convert it into a `u32`.
+    let version = version.get(0)
+        .unwrap_or(&String::from("1"))
+        .trim_start_matches("-v").to_string() // Remove the "-v" prefix of the flag.
+        .parse::<u32>()
+        .expect("Cannot parse the version flag into a unsigned integer.");
+    
+    assert!(version >= 1 && version <= 40, "Invalid Version.");
+    
     if cfg!(debug_assertions) {
         // Debug Info
         println!("Message: {:?}", message);
         println!("Output Path: {:?}", output_path);
         println!("Flags: {:?}", flags);
+        println!("Version: {:?}", version);
     }
 
-    let qr_code = encode::generate(message);
+    let qr_code = encode::generate(message, version);
     qr_code.save(output_path.clone())
         .expect(format!("Failed to save QR Code to {:?}", output_path).as_str());
 }
